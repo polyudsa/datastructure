9231aa542d408f1bc703880d481989e4383391f7
HQCfly <2249629867@qq.com>
2021-09-03 22:25:32 +0800

    By evil, add test

---
+++ BinarySearchTrees/evil/Test.java
@@ -0,0 +1,8 @@
+package BinarySearchTrees.evil;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/9/3 10:23 下午
+ */
+public class Test {
+}
---
+++ BinarySearchTrees/gleam/Test.java
@@ -0,0 +1,8 @@
+package BinarySearchTrees.gleam;
+
+/**
+ * @Author: Twiss
+ * @Date: 2021/9/3 10:24 下午
+ */
+public class Test {
+}
