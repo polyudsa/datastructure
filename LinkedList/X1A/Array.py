9ca96dd5df48e6605290962131f7b3b87c3079c7
X1AOX1A <1764400017@e.gzhu.edu.cn>
2021-09-08 10:12:12 +0800

    By X1A, Add Array

--- LinkedList/X1A/Array.py
+++ LinkedList/X1A/Array.py
@@ -3,93 +3,91 @@ class Array(object):
     
     Args:
     - values: list, the values of array
-    - capacity: int, the capacity of array
-    - fillValue: the default value of array
+    - CAPACITY: int, the capacity of array
+    - FILL_VALUE: the default value of array
         
     Attributes:
     - data: list, the data of array
-    - logicalSize: int, the logical size of array, start from 1
-    - capacity: int, the physical size of array, start from 1
+    - logical_size: int, the logical size of array, start from 1
+    - CAPACITY: int, the physical size of array, start from 1
     '''
 
-    def __init__(self, values=None, capacity=10, fillValue=None):
+    def __init__(self, values=None, CAPACITY=10, FILL_VALUE=None):
         # O(n)
         self.data = list()
-        for count in range(capacity):
-            self.data.append(fillValue)
+        for count in range(CAPACITY):
+            self.data.append(FILL_VALUE)
         if values is not None:
             for i, value in enumerate(values):             # O(n)
                 self.data[i] = value
-            self.logicalSize = i+1          # the logical size of array
+            self.logical_size = i+1          # the logical size of array
         else:
-            self.logicalSize = 0            # the logical size of array
+            self.logical_size = 0            # the logical size of array
             
-        self.capacity = capacity            # the physical size of array
+        self.CAPACITY = CAPACITY            # the physical size of array
         
     def print_array(self):
         # O(n)
-        for i in range(self.logicalSize):
+        for i in range(self.logical_size):
             print(self.data[i])
             
     def append(self):
         # O(n)
         # when the array is full, double the capacity
-        temp = Array(None, self.capacity*2)
-        for i in range(self.logicalSize):                   # O(n)
+        temp = Array(None, self.CAPACITY*2)
+        for i in range(self.logical_size):                   # O(n)
             temp.data[i] = self.data[i]
         self.data = temp.data
-        self.capacity = temp.capacity
+        self.CAPACITY = temp.CAPACITY
         
     def subtract(self):
         # O(n)
         # when logical size <= capacity // 4, reducing the capacity to one-half
-        temp = Array(None, self.capacity//2)
-        for i in range(self.logicalSize):                   # O(n)
+        temp = Array(None, self.CAPACITY//2)
+        for i in range(self.logical_size):                   # O(n)
             temp.data[i] = self.data[i]
         self.data = temp.data
-        self.capacity = temp.capacity
+        self.CAPACITY = temp.CAPACITY
         
-    def insert(self, newItem, targetIndex):
+    def insert(self, new_item, target_index):
         # O(n)
         '''Insert new item to target index
         
         Args: 
-        - newItem: the item to insert
-        - targetIndex: int, the target index of new item, start from 0
-        
+        - new_item: the item to insert
+        - target_index: int, the target index of new item, start from 0
         '''
         # when the array is full, double the capacity
-        if self.capacity - self.logicalSize == 0:
+        if self.CAPACITY = self.logical_size:
             self.append()                                   # O(n)
-        for i in range(self.logicalSize, targetIndex, -1):  # O(n)
+        for i in range(self.logical_size, target_index, -1):  # O(n)
             self.data[i] = self.data[i-1]
-        self.data[targetIndex] = newItem
-        self.logicalSize += 1
+        self.data[target_index] = new_item
+        self.logical_size += 1
         
-    def delete(self, targetIndex):
+    def delete(self, target_index):
         # O(n)
         '''Delete the item of target index
         
         Args:
-        - targetIndex: int, the target index of deleting item, start from 0
-        
+        - target_index: int, the target index of deleting item, start from 0
         '''
-        if self.logicalSize - targetIndex < 1:
+        if self.logical_size - target_index < 1:
             raise Exception("Error! The index is out of range")
 
-        for i in range(targetIndex, self.logicalSize, 1):   # O(n)
+        for i in range(target_index, self.logical_size, 1):   # O(n)
             self.data[i] = self.data[i+1]
-        self.data[self.logicalSize] = None
-        self.logicalSize -= 1
+        self.data[self.logical_size] = None
+        self.logical_size -= 1
         
         # when logical size <= capacity // 4, reducing the capacity to one-half
-        if  self.logicalSize <= self.capacity// 4:  
+        if  self.logical_size <= self.CAPACITY// 4:  
             self.subtract()                                 # O(n)
             
     def array_max(self):
         # O(n)
-        currentMax = self.data[0]
-        for i in range(1, self.logicalSize):                # O(n)
-            if self.data[i] > currentMax:
-                currentMax = self.data[i]
-        return currentMax
+        current_max = self.data[0]
+        for i in range(1, self.logical_size):                # O(n)
+            if self.data[i] > current_max:
+                current_max = self.data[i]
+        return current_max
