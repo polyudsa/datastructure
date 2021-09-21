fb46400770fb08ea10f18e0a322cb984c8d7fc02
X1AOX1A <1764400017@e.gzhu.edu.cn>
2021-09-21 20:22:34 +0800

    By X1A, Add Array

--- LinkedList/X1A/Array.py
+++ LinkedList/X1A/Array.py
@@ -2,68 +2,59 @@ class Array(object):
     ''' Implementation of array
     
     Args:
-    - values: list, the values of array
-    - capacity: int, the capacity of array
-    - fill_value: the default value of array
+    - data: list, the elements of array
+    - CAPACITY: int, the physical size of array
+    - FILL_VALUE: default None, the default value of array
         
     Attributes:
-    - data: list, the data of array
-    - logical_size: int, the logical size of array, start from 1
-    - capacity: int, the physical size of array, start from 1
+    - data: list, the elements of array
+    - len: int, the logical size of array
+    - CAPACITY: int, the physical size of array, start from 1
+    - FILL_VALUE: default None, the default value of array
+    
     '''
+    
 
-    def __init__(self, values=None, capacity=10, fill_value=None):
+    def __init__(self, data=None, CAPACITY=10, FILL_VALUE=None):
         # O(n)
         self.data = list()
-        for count in range(capacity):
-            self.data.append(fill_value)
-        if values is not None:
-            for i, value in enumerate(values):             # O(n)
-                self.data[i] = value
-            self.logical_size = i+1          # the logical size of array
+        self.FILL_VALUE = FILL_VALUE
+        for count in range(CAPACITY):
+            self.data.append(FILL_VALUE)
+        if data is not None:
+            for i, element in enumerate(data):             # O(n)
+                self.data[i] = element
+            self.len = i+1                   # the logical size of array
         else:
-            self.logical_size = 0            # the logical size of array
+            self.len = 0                     # the logical size of array
             
-        self.capacity = capacity            # the physical size of array
+        self.CAPACITY = CAPACITY             # the physical size of array
         
-    def print_array(self):
-        # O(n)
-        for i in range(self.logical_size):
-            print(self.data[i])
-            
-    def append(self):
-        # O(n)
-        # when the array is full, double the capacity
-        temp = Array(None, self.capacity*2)
-        for i in range(self.logical_size):                   # O(n)
-            temp.data[i] = self.data[i]
-        self.data = temp.data
-        self.capacity = temp.capacity
         
-    def subtract(self):
-        # O(n)
-        # when logical size <= capacity // 4, reducing the capacity to one-half
-        temp = Array(None, self.capacity//2)
-        for i in range(self.logical_size):                   # O(n)
-            temp.data[i] = self.data[i]
-        self.data = temp.data
-        self.capacity = temp.capacity
-        
-    def insert(self, new_item, target_index):
+    def insert(self, data, target_index):
         # O(n)
         '''Insert new item to target index
         
         Args: 
-        - new_item: the item to insert
+        - data: the item to insert
         - target_index: int, the target index of new item, start from 0
+        
         '''
-        # when the array is full, double the capacity
-        if self.capacity == self.logical_size:
-            self.append()                                   # O(n)
-        for i in range(self.logical_size, target_index, -1):  # O(n)
+        if self.CAPACITY == self.len:
+            raise Exception("Error! The array is full")
+            
+        if (target_index > self.len) and (self.len != 0):
+            raise Exception("Error! The index is out of range")
+        elif target_index < 0:
+            if -target_index > self.len +1 :
+                raise Exception("Error! The index is out of range")
+            target_index = target_index + self.len + 1
+            
+        for i in range(self.len, target_index, -1):  # O(n)
             self.data[i] = self.data[i-1]
-        self.data[target_index] = new_item
-        self.logical_size += 1
+        self.data[target_index] = data
+        self.len += 1
+        
         
     def delete(self, target_index):
         # O(n)
@@ -71,23 +62,26 @@ class Array(object):
         
         Args:
         - target_index: int, the target index of deleting item, start from 0
-        '''
-        if self.logical_size - target_index < 1:
-            raise Exception("Error! The index is out of range")
-
-        for i in range(target_index, self.logical_size, 1):   # O(n)
-            self.data[i] = self.data[i+1]
-        self.data[self.logical_size] = None
-        self.logical_size -= 1
         
-        # when logical size <= capacity // 4, reducing the capacity to one-half
-        if  self.logical_size <= self.capacity// 4:  
-            self.subtract()                                 # O(n)
+        '''
+        if target_index+1 > self.len:
+            raise Exception("Error! The index is out of range")
+        elif target_index < 0:
+            if -target_index > self.len:
+                raise Exception("Error! The index is out of range")
+            target_index += self.len
+
+        for i in range(target_index, self.len-1):   # O(n)
+            self.data[i] = self.data[i+1]
+
+        self.data[self.len-1] = self.FILL_VALUE
+        self.len -= 1
+
             
     def array_max(self):
-        # O(n)
-        current_max = self.data[0]
-        for i in range(1, self.logical_size):                # O(n)
-            if self.data[i] > current_max:
-                current_max = self.data[i]
-        return current_max
+        # O(n) sequential search
+        currentMax = self.data[0]
+        for i in range(1, self.len):                # O(n)
+            if self.data[i] > currentMax:
+                currentMax = self.data[i]
+        return currentMax
--- LinkedList/X1A/Array_Test.py
+++ LinkedList/X1A/Array_Test.py
@@ -2,72 +2,31 @@ from Array import Array
 
 # Create a new array: [1, 2, 3, 4, 5]
 test_list = [1, 2, 3, 4, 5]
-a = Array(test_list, capacity=test_list.__len__())
-assert test_list==a.data
-assert test_list.__len__()==a.capacity==a.logical_size
+a = Array(test_list, CAPACITY = test_list.__len__())
+print('Original array:', a.data)
+print('Capacity:', a.CAPACITY)
+print('Len:', a.len)
+assert a.data == test_list
+assert a.CAPACITY == a.len == test_list.__len__()
 
-print('Origin array is: ')
-print(a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
+# Delete data at index 2
+a.delete(2)
+assert a.data == [1, 2, 4, 5, None]
+assert a.CAPACITY == 5
+assert a.len == 4
+print('\nDelete index 2:', a.data)
+print('Capacity:', a.CAPACITY)
+print('Len:', a.len)
 
+# Insert 100 at last
+a.insert(100, -1)
+assert a.data == [1, 2, 4, 5, 100]
+assert a.CAPACITY == 5
+assert a.len == 5
+print('\nInsert 100 at last:', a.data)
+print('Capacity:', a.CAPACITY)
+print('Len:', a.len)
 
-print('\n+++++++++++++++++++++++')
-print('++++++ Insertion ++++++')
-print('+++++++++++++++++++++++')
-
-# Insert 5 at the head
-print('\nInsert 5 at the head')
-print('Before: ', a.data)
-a.insert(5, 0)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
-
-# Insert 9 at the tail
-print('\nInsert 9 at the tail')
-print('Before: ', a.data)
-a.insert(9, 6)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
-
-# Double the capacity if the capacity is full at the time of insertion
-print('\nDouble the capacity if the capacity is full at the time of insertion')
-print('Insert 10 at the tail')
-print('Before: ', a.data)
-a.insert(10, 7)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
-
-
-print('\n++++++++++++++++++++++++')
-print('+++++++ Deletion +++++++')
-print('++++++++++++++++++++++++')
-
-# Delete the head
-print('\nDelete the head')
-print('Before: ', a.data)
-a.delete(0)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
-
-# Delete the tail
-print('\nDelete the tail')
-print('Before: ', a.data)
-a.delete(6)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
-
-# When logical size <= capacity // 4, reducing the capacity to one-half
-print('\nWhen logical size <= capacity // 4, reducing the capacity to one-half')
-a.delete(0)
-a.delete(0)
-print('Before: ', a.data)
-a.delete(0)
-print('After:  ', a.data)
-print('Logical size is: ', a.logical_size)
-print('Physical size is ', a.capacity)
+# Find the max
+assert a.array_max() == 100
+print('\nThe maximum element of array:', a.array_max())
